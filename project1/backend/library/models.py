70af823d22b0acc54ab21b9b83c79936df943590
Sagar Tamang <sagar.bdr0000@gmail.com>
2024-04-30 19:57:01 +0530

    latest CRUD app completed

--- project1/backend/library/models.py
+++ project1/backend/library/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Library(models.Model):
-  id = models.IntegerField(max_length=155, primary_key=True)
+  id = models.IntegerField(primary_key=True)
   name = models.CharField(max_length=155)
 
   
--- project1/backend/main/api.py
+++ project1/backend/main/api.py
@@ -1,12 +1,20 @@
-from ninja import NinjaAPI, Schema
+from ninja import NinjaAPI, Schema, ModelSchema
 from library.models import Library
 from django.shortcuts import get_object_or_404
 
 api = NinjaAPI()
 
-class LibrarySchema(Schema):
-  id: int
-  name: str
+# class LibrarySchema(Schema):
+#   id: int
+#   name: str
+
+# We can instead use Model Schema so that the schema can be automatically creted from the model
+  
+class LibrarySchema(ModelSchema):
+  class Meta:
+    model = Library
+    # fields = "__all__"
+    fields = ['id', 'name']
 
 @api.get("hello/")
 def helloworld(request):
@@ -20,3 +28,30 @@ def library_detail(request, id: int):
 @api.get("/library", response=list[LibrarySchema])
 def library_list(request):
   return Library.objects.all()
+
+@api.post("/library", response=LibrarySchema)
+def create_library(request, payload: LibrarySchema):
+  library = Library.objects.create(
+    id= payload.id,
+    name= payload.name,
+  )
+  # library = Library.objects.create(
+  #   **payload.dict()
+  # )
+  return library
+
+@api.delete("/library/{id}")
+def delete_book(request, id: int):
+  library = get_object_or_404(Library, id=id)
+  library.delete()
+  return {'success': True}
+
+@api.patch("/library/{id}", response=LibrarySchema)
+def update_book(request, id: int, payload: LibrarySchema):
+  library = get_object_or_404(Library, id=id)
+
+  for attr, value in payload.dict(exclude_unset=True).items():
+    setattr(library, attr, value)
+  
+  library.save()
+  return library 
